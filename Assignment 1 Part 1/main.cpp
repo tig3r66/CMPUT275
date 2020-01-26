fcefdb284acf4071372fd44efa6630dd3d8c532c
tig3r66 <eguo1@ualberta.ca>
2020-01-25 17:09:29 -0700

    fixed indentation

--- Assignment 1 Part 1/main.cpp
+++ Assignment 1 Part 1/main.cpp
@@ -1,11 +1,11 @@
 //  =======================================
-//	Name: Edward (Eddie) Guo
-//	ID: [redacted]
-//	Partner: Jason Kim
-// 	CMPUT 275, Winter 2020
+//  Name: Edward (Eddie) Guo
+//  ID: [redacted]
+//  Partner: Jason Kim
+//  CMPUT 275, Winter 2020
 //
-// 	Assignment 1 Part 1: Restaurant Finder
-// 	=======================================
+//  Assignment 1 Part 1: Restaurant Finder
+//  =======================================
 
 #include <Arduino.h>
 
