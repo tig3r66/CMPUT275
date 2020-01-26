115094bdf34b3e99417326b3bf10b4de9081aa4e
tig3r66 <eguo1@ualberta.ca>
2020-01-25 17:08:39 -0700

    fixed indentation

--- Assignment 1 Part 1/main.cpp
+++ Assignment 1 Part 1/main.cpp
@@ -1,25 +1,25 @@
 //  =======================================
-//  Name: Edward (Eddie) Guo
-//  ID: [redacted]
+//	Name: Edward (Eddie) Guo
+//	ID: [redacted]
 //	Partner: Jason Kim
-//  CMPUT 275, Winter 2020
+// 	CMPUT 275, Winter 2020
 //
-//  Assignment 1 Part 1: Restaurant Finder
-//  =======================================
+// 	Assignment 1 Part 1: Restaurant Finder
+// 	=======================================
 
 #include <Arduino.h>
 
 
 int main() {
-	// main code here
+    // main code here
 
-	Serial.end();
-	return 0;
+    Serial.end();
+    return 0;
 }
 
 
 void setup() {
-	init();
-	Serial.begin(9600);
-	// configure more here
+    init();
+    Serial.begin(9600);
+    // configure more here
 }
