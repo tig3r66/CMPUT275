be8c39f3d6b348d2b7a95076bc747fad721cf38f
tig3r66 <eguo1@ualberta.ca>
2020-01-25 17:06:46 -0700

    created started files

---
+++ Assignment 1 Part 1/main.cpp
@@ -0,0 +1,24 @@
+//  =======================================
+//  Name: Edward (Eddie) Guo
+//  ID: 1576381
+//  CMPUT 275, Winter 2020
+//
+//  Assignment 1 Part 1: Restaurant Finder
+//  =======================================
+
+#include <Arduino.h>
+
+
+int main() {
+	// main code here
+
+	Serial.end();
+	return 0;
+}
+
+
+void setup() {
+	init();
+	Serial.begin(9600);
+	// configure more here
+}
