601a35f6e0345d771fe370795ab639ee028586c2
tig3r66 <eguo1@ualberta.ca>
2020-01-29 16:21:26 -0700

    helper code from previous weekly exercises

---
+++ Helper Weekly Exercices/Display and Joystick/joy_cursor/build-mega2560/userlibs/Adafruit_GFX/glcdfont.c.d
@@ -0,0 +1,2 @@
+build-mega2560/userlibs/Adafruit_GFX/glcdfont.c.o: \
+ /home/cmput274/arduino-ua/libraries/Adafruit_GFX/glcdfont.c
---
+++ Helper Weekly Exercices/Restaurants and Pointers/good_copy/build-mega2560/userlibs/SD/utility/SdVolume.cpp.d
@@ -0,0 +1,11 @@
+build-mega2560/userlibs/SD/utility/SdVolume.cpp.o: \
+ /home/cmput274/arduino-ua/libraries/SD/utility/SdVolume.cpp \
+ /home/cmput274/arduino-ua/libraries/SD/utility/SdFat.h \
+ /home/cmput274/arduino-ua/libraries/SD/utility/Sd2Card.h \
+ /home/cmput274/arduino-ua/libraries/SD/utility/Sd2PinMap.h \
+ /home/cmput274/arduino-ua/libraries/SD/utility/SdInfo.h \
+ /home/cmput274/arduino-ua/libraries/SD/utility/FatStructs.h \
+ /usr/share/arduino/hardware/arduino//cores/arduino/Print.h \
+ /usr/share/arduino/hardware/arduino//cores/arduino/WString.h \
+ /usr/share/arduino/hardware/arduino//cores/arduino/Printable.h \
+ /usr/share/arduino/hardware/arduino//cores/arduino/new.h
