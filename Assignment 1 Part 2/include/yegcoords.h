66d1483b42ddbf7504e4f22f0d052e59e2971fe0
tig3r66 <eguo1@ualberta.ca>
2020-02-13 09:04:17 -0700

    added function declarations

--- Assignment 1 Part 2/include/yegcoords.h
+++ Assignment 1 Part 2/include/yegcoords.h
@@ -10,12 +10,22 @@
 #define _YEGCOORDS_H_
 
 
+// ================================= CONSTANTS =================================
 #define MAP_WIDTH 2048
 #define MAP_HEIGHT 2048
 #define LAT_NORTH 5361858l
 #define LAT_SOUTH 5340953l
 #define LON_WEST -11368652l
 #define LON_EAST -11333496l
+
+
+// =========================== FUNCTION DECLARATIONS ===========================
+// these functions convert between x/y to lat/lon in in the map of Edmonton and
+// vice versa
+int32_t x_to_lon(int16_t x);
+int32_t x_to_lon(int16_t x);
+int16_t lon_to_x(int32_t lon);
+int16_t lat_to_y(int32_t lat);
 
 
 #endif
