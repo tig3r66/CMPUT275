f81f4c0dde39682b77cfbf67fecddaf6f558d843
Eddie Guo <eguo1@ualberta.ca>
2020-03-06 15:35:21 -0700

    added controller class

--- Final Project/main.py
+++ Final Project/main.py
@@ -1,127 +1,21 @@
 import sys
-import random
-import worker_threads as wt
-from collections import deque
-
-# for data streaming
-from pylsl import StreamInlet, resolve_stream
 
 # for UI
 from PyQt5 import QtCore, QtWidgets
-from mpl_canvas import MplCanvas
-
-# for data plotting
-import matplotlib.pyplot as plt
-plt.style.use('seaborn')
+from plot_window import PlotWindow
 
 
-class MainWindow(QtWidgets.QMainWindow):
-    _run_thread = True
+class Controller:
 
-    def __init__(self, *args, **kwargs):
-        super(MainWindow, self).__init__(*args, **kwargs)
-
-        random.seed(0)
-        self.setup_ui()
-
-        n_data = 100
-        self.xdata = deque([-(n_data/10 - i/10) for i in range(n_data)])
-        self.ydata = deque([random.uniform(-64, 64) for i in range(n_data)])
-
-        # Storing a reference to the plotted line
-        self._plot_ref = None
-        self.show()
-
-        # multithreading
-        self.threadpool = QtCore.QThreadPool()
-        self.pull_data_worker()
-        # gracefully exiting all threads
-        app.aboutToQuit.connect(self.stop_threads)
-
-        # Setup a timer to trigger the redraw
-        self.timer = QtCore.QTimer()
-        self.timer.setInterval(100)
-        self.timer.timeout.connect(self.eeg_worker)
-        self.timer.start()
-
-
-    def setup_ui(self):
-        self.setStyleSheet(
-            'QMainWindow {'
-            '   color: white;'
-            '   background-color: white;'
-            '}'
-        )
-        # main window
-        self.setWindowTitle('EEG Visualizer')
-        self._main = QtWidgets.QWidget()
-        # eeg and fft windows
-        self.setup_eeg_window()
-        self.setup_fft_window()
-
-        # layout
-        layout = QtWidgets.QGridLayout(self._main)
-        self.setCentralWidget(self._main)
-        layout.addWidget(self.eeg_canvas, 0, 0)
-        layout.addWidget(self.fft_canvas, 0, 1)
-
-
-    def setup_eeg_window(self):
-        self.eeg_canvas = MplCanvas(self, width=9, height=3, dpi=100)
-        self.eeg_canvas.axes.set_ylim(-150, 150)
-        self.eeg_canvas.axes.set_title('Raw Time Series')
-        self.eeg_canvas.axes.set_xlabel('Time (s)')
-        self.eeg_canvas.axes.get_yaxis().set_visible(False)
-        self.eeg_canvas.fig.tight_layout()
-
-
-    def setup_fft_window(self):
-        self.fft_canvas = MplCanvas(self, width=6, height=3, dpi=100)
-        self.fft_canvas.axes.set_title('FFT Plot')
-        self.fft_canvas.axes.set_xlabel('Frequency (Hz)')
-        self.fft_canvas.axes.get_yaxis().set_visible(False)
-        self.fft_canvas.fig.tight_layout()
-
-
-    def update_eegplot(self):
-        self.ydata.popleft()
-        self.ydata.append(random.uniform(-64, 64))
-
-        if self._plot_ref is None:
-            self._plot_ref, = self.eeg_canvas.axes.plot(self.xdata,
-                self.ydata, 'k')
-        else:
-            self._plot_ref.set_ydata(self.ydata)
-
-        # trigger the canvas to update and redraw
-        self.eeg_canvas.draw()
-
-
-    def eeg_worker(self):
-        worker = wt.Worker(self.update_eegplot)
-        self.threadpool.start(worker)
-
-
-    def pull_data(self):
-        # TODO: make this update a buffer to plot from
-        print('Looking for an EEG stream...')
-        streams = resolve_stream('type', 'EEG')
-        inlet = StreamInlet(streams[0])
-        while self._run_thread:
-            sample, timestamp = inlet.pull_sample()
-            print(timestamp, sample)
-
-
-    def pull_data_worker(self):
-        self.data_worker = wt.Worker(self.pull_data)
-        self.threadpool.start(self.data_worker)
-
-
-    def stop_threads(self):
-        self._run_thread = False
+    def show_plot_window(self):
+        self.plot_window = QtWidgets.QMainWindow()
+        self.ui = PlotWindow()
+        self.ui.setup_ui(self.plot_window)
+        self.plot_window.show()
 
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
-    w = MainWindow()
+    controller = Controller()
+    controller.show_plot_window()
     sys.exit(app.exec_())
