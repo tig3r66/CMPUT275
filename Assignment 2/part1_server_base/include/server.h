310907964b279ef4f826fbe13b63912d9ea35b62
Eddie Guo <eguo1@ualberta.ca>
2020-03-12 16:58:02 -0600

    cleaned some stuff up

--- Assignment 2/part1_server_base/include/server.h
+++ Assignment 2/part1_server_base/include/server.h
@@ -3,8 +3,10 @@
 
 #include "wdigraph.h"
 #include <unordered_map>
+
 #define SCALE 100000
 
+// stores a point read from the map in 100,000-ths of a degree as integers
 struct Point {
 	long long lat;
 	long long lon;
@@ -17,5 +19,7 @@ void processRequest(long, long, long, long, WDigraph&);
 long long manhattan(const Point&, const Point&);
 
 void readGraph(string file, WDigraph&, unordered_map<int, Point>);
+
+void isValidIfstream(const ifstream& filename);
 
 #endif
--- Assignment 2/part1_server_base/server.cpp
+++ Assignment 2/part1_server_base/server.cpp
@@ -11,9 +11,9 @@ using namespace std;
 
 void hold() {
 	char comm;
-	//bool commed = true; //timeout implmentation for part 2
+	//bool commed = true; // timeout implmentation for part 2
 
-	while (true) { //add conditon regrading time for part 2
+	while (true) { // add conditon regrading time for part 2
 		cin >> comm;
 		if (comm == 'A') { // wait for ACK key 
 			//commed = true;
@@ -31,8 +31,8 @@ void processRequest(long startLon, long startLat, long endLon, long endLat, WDig
 	// calc and print waypoints in a list or shit
 	unordered_map<int, PIL> tree;
 	Point minpoint, maxpoints;
-	for (auto x: )
-	dijkstra(graph, )
+	// for (auto x: )
+	// dijkstra(graph, )
 	/*
 	if () { // if waypoints = 0
 		return;
@@ -45,7 +45,7 @@ void processRequest(long startLon, long startLat, long endLon, long endLat, WDig
 		hold(); //wait for ACK
 	}
 	*/
-	cout << 'E' << endl; // end key, replace
+	// cout << 'E' << endl; // end key, replace
 }
 
 long long manhattan(const Point& pt1, const Point& pt2) {
@@ -53,20 +53,27 @@ long long manhattan(const Point& pt1, const Point& pt2) {
 }
 
 
-void readGraph(string filename, WDigraph &graph, unordered_map<int, Point> points) {
+void isValidIfstream(const ifstream& filename) {
+    if (!filename.good()) {
+	    cout << "error: digraph text file not found" << endl;
+	    cout << "usage: ./graph_concepts <digraph_file_name>.txt" << endl;
+	    exit(EXIT_FAILURE);
+    }
+}
+
+
+void readGraph(string filename, WDigraph &graph, unordered_map<long long, Point> points) {
 	char graphID, comma;
     int ID, start, end;
 	float lat, lon;
-	long long convertedLat, convertedLon;
+	long long convertedLat, convertedLon, distance;
 	string name;
-	ifstream textIn(filename);
-    if (!textIn.good()) {
-        cout << "error: digraph text file not found" << endl;
-        cout << "usage: ./graph_concepts <digraph_file_name>.txt" << endl;
-        exit(EXIT_FAILURE);
-    }
     Point point;
-    long long distance;
+
+	// input validation
+	ifstream textIn(filename);
+    isValidIfstream(textIn);
+
 	while (textIn >> graphID >> comma) {
         if (graphID == 'V') {
             // vertex format: V,ID,Lat,Lon
@@ -74,15 +81,14 @@ void readGraph(string filename, WDigraph &graph, unordered_map<int, Point> point
             convertedLon = static_cast<long long>(lon * SCALE);
             convertedLat = static_cast<long long>(lat * SCALE);
             point = {convertedLat, convertedLon};
-            points[ID] = point;
+            points.second = point;
             graph.addVertex(ID);
         } else if (graphID == 'E') {
             // edge format: E,start,end,name
             textIn >> start >> comma >> end >> comma;
             getline(textIn, name);
-            //find points and get manhattan distance
+            // find points and get manhattan distance
             distance = manhattan(points[start], points[end]);
-            //associate edge with that distance
             graph.addEdge(start, end, distance); 
         }
     }
@@ -90,6 +96,9 @@ void readGraph(string filename, WDigraph &graph, unordered_map<int, Point> point
 
 
 int main(int argc, char* argv[]) {
+    WDigraph graph;
+    Point points;
+
 	readGraph(argv[1], graph, points);
 	char input;
 	long startLon, startLat, endLon, endLat;
@@ -99,7 +108,7 @@ int main(int argc, char* argv[]) {
 			cin >> startLon >> startLat >> endLon >> endLat; //replace with serial input in part 2
 			processRequest(startLon, startLat, endLon, endLat, graph); //do djks
 		}
-	}	
+	}
 
 	return 0;
 }
