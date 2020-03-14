18e4f0f1b504f6dad33ad9d686788ec6055f12f2
jaskim9824 <jaskim9824@gmail.com>
2020-03-14 14:25:35 -0600

    added .py script to check if manhattan distances are correct

---
+++ Assignment 2/part1_server_base/filechecker.py
@@ -0,0 +1,40 @@
+import sys
+
+def manhattan(x1, y1, x2, y2):
+	return abs(x1-x2) + abs(y1-y2)
+
+fin1 = open(sys.argv[1], "r")
+fin2 = open(sys.argv[2], "r")
+fout = open("output.txt", "w")
+fin1.read(1)
+counter1 = int(fin1.readline())
+fin2.read(1)
+counter2 = int(fin2.readline())
+if (counter1 != counter2):
+	raise Exception("number of lines do not match")
+
+i = 0
+sum1 = 0
+sum2 = 0
+while (i < counter1):
+	line = fin1.readline()
+	if (i == 0):
+		prevLat1 = int(line[2:9])
+		prevLon1 = int(line[10:19])
+		prevLat2 = int(line[2:9])
+		prevLon2 = int(line[10:19])
+		i += 1
+	else:
+		currentLat1 = int(line[2:9])	
+		currentLon1 = int(line[10:19])	
+		currentLat2 = int(line[2:9])
+		currentLon2 = int(line[10:19])
+		sum1 +=  manhattan(prevLon1, prevLat1, currentLon1, currentLat1)
+		sum2 += manhattan(prevLon2, prevLat2, currentLon2, currentLat2)
+		i += 1
+
+fout.write(str(sum1 == sum2))
+
+fin1.close()
+fin2.close()
+fout.close()
