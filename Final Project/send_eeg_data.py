9b31e1fff69fa01bcf8f0a3885524d5d74d1c39d
Eddie Guo <eguo1@ualberta.ca>
2020-03-15 15:50:54 -0600

    updated the data to send by using real EEG data

--- Final Project/send_eeg_data.py
+++ Final Project/send_eeg_data.py
@@ -1,26 +1,39 @@
-"""Example program to demonstrate how to send a multi-channel time series to
-LSL."""
-
 import time
-import random
-
+import numpy as np
 from pylsl import StreamInfo, StreamOutlet
 
-# Creating a stream with the following properties:
-#   name = BioSemi
-#   signal type = EEG
-#   number of channels = 1
-#   frequency = 100 Hz
-#   data type = float32
-#   unique identifier = eeg_project_123
+
+"""
+Creating a stream with the following properties:
+    name = BioSemi
+    signal type = EEG
+    number of channels = 1 (the first "channel" is the timestamp)
+    frequency = 100 Hz
+    data type = float32
+    unique identifier = eeg_project_123
+"""
 info = StreamInfo('BioSemi', 'EEG', 1, 100, 'float32', 'eeg_project_123')
 
-# data outlet
+
+# DATA OUTLET
 outlet = StreamOutlet(info)
 
+
+# RETRIEVING DATA
+eeg_data = []
+with open('eeg_data/data.txt', 'r') as fin:
+    for i in fin:
+        eeg_data.append(float(i.strip()))
+data_length = len(eeg_data)
+
+
+# SENDING DATA
 print("now sending data...")
+line_counter = 0
 while True:
-    # make a new random 1-channel sample and send it
-    mysample = [random.uniform(-64, 64)]
+    # make a new 2-channel sample and send it
+    mysample = [eeg_data[line_counter]]
     outlet.push_sample(mysample)
+
+    line_counter = (line_counter + 1) % data_length
     time.sleep(0.01)
